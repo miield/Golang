21b54847c9b17c9b71de9926cda9662763dd0166
miield <aodasola95@gmail.com>
2023-01-19 23:51:33 +0100

    Variables 1

--- 02Variables/main.go
+++ 02Variables/main.go
@@ -2,8 +2,32 @@ package main
 
 import "fmt"
 
+// Variables Types
+
 func main() {
 	var age int = 30
 	fmt.Println(age)
-	fmt.Printf("Variable is a type: %T", age)
+	fmt.Printf("Variable is a type: %T \n", age)
+
+	var name string = "Oyindamola Abiola"
+	fmt.Println(name)
+	fmt.Printf("Variable is a type: %T \n", name)
+
+	var isFemale bool = true
+	fmt.Println(isFemale)
+	fmt.Printf("Variable is a type: %T \n", isFemale)
+
+	var smallVal uint8 = 255 // 256 overflows
+	fmt.Println(smallVal)
+	fmt.Printf("Variable is a type: %T \n", smallVal)
+
+	var smallFloat float32 = 255.5678268289398 // float32 5decimals : float64 long decimal
+	fmt.Println(smallFloat)
+	fmt.Printf("Variable is a type: %T \n", smallFloat)
 }
+
+// func main2() {
+// 	var name string = "Oyindamola Abiola"
+// 	fmt.Println(name)
+// 	fmt.Printf("Variable is a type: %T \n", name)
+// }
